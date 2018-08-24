4da86f82ef40164f35893cd79a8ed1b40cab125f
Jordan Yu <jordanyu44@gmail.com>
2018-08-24 11:44:22 -0700

    Unignore bundle.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 // });
 
 app.get('/', (request, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, './index.html'))
 });
 
 const yelp = require('yelp-fusion');
