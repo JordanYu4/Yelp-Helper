6240f60239f7a7d326b01fdaa7b7e93ddc68232b
Jordan Yu <jordanyu44@gmail.com>
2018-08-24 10:43:58 -0700

    Style chart

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -11,50 +11,62 @@ let yelpChart = new Chart(ctx, {
     datasets: [{
       label: 'Businesses',
       data: [{
-        x: 20,
-        y: 8
+        x: 24,
+        y: 3.2
       }, {
         x: 5,
-        y: 34
+        y: 4.3
       }, {
         x: 10,
-        y: 5
+        y: 4.8
       }, {
         x: 4,
-        y: 15
+        y: 2.3
       }, {
         x: 7,
-        y: 22
+        y: 4.1
       }],
-      pointRadius: 3,
-      pointHitRadius: 4,
+      pointRadius: 5,
+      pointStyle: 'rectRounded',
+      pointHitRadius: 6,
       backgroundColor: 'red',
       borderWidth: 1,
       borderColor: 'red',
       hoverBorderWidth: 2,
-      hoverBorderColor: '#000'
+      hoverBorderColor: '#777'
     }]
   },
   options: {
     title: {
       display: true,
       text: 'Businesses nearby',
-      fontSize: 24
+      fontSize: 24,
+      lineHeight: 1.4,
+      padding: 10
     },
     scales: {
       xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Distance (mi)',
+        },
         type: 'linear',
         position: 'bottom',
         ticks: {
-          beginAtZero: true
+          beginAtZero: true,
+          max: 25
         }
       }],
       yAxes: [{
-
         type: 'linear',
         position: 'left',
         ticks: {
-          beginAtZero: true
+          beginAtZero: true,
+          max: 5,
+          stepSize: 1,
+          callback: function(value, index, values) {
+            return value + ' ⭐';
+          }
         }
       }]
     },
@@ -69,9 +81,9 @@ let yelpChart = new Chart(ctx, {
     layout: {
       padding: {
         top: 5,
-        right: 5,
-        bottom: 10,
-        left: 10
+        right: 30,
+        bottom: 15,
+        left: 20
       }
     }
   }
