072259d6ce5a909d2a42dd9ccfe5ab642d99feb4
Jordan Yu <jordanyu44@gmail.com>
2018-09-18 15:12:21 -0700

    Remove jquery ajax from application

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,7 @@ import yelpChart from './chart.js';
 
 import '../assets/stylesheets/index.scss';
 
-const searchInput = form => ({
+const formInput = form => ({
   term: form[0].value,
   location: form[1].value
 });
@@ -11,6 +11,7 @@ const searchInput = form => ({
 document.addEventListener('DOMContentLoaded', () => {
   
   let isbn = '0201558025';
+
   axios.get(`/books/${isbn}`)
   .then((response) => {
     console.log(response);
@@ -22,43 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener("submit", e => {
     e.preventDefault();
     const searchForm = document.getElementById("search-form");
-    const newSearch = searchInput(searchForm); // declare as var outside of DOMContentLoaded?
-    console.log(newSearch);
+    const searchParams = formInput(searchForm); 
+    console.log(searchParams);
 
-    // $.ajax({
-      //   url: "/yelp",
-      //   type: "get",
-      //   success: response => {
-        //     console.log("Retrieving response...");
-        //     return response;
-        //   }
-    // });
-
+    axios.get(`/search`, searchParams)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
   });
-
 });
       
     // chart rendering logic?
-    
-    // let searchParams = {
-    //   term: 'four barrel coffee',
-    //   location: 'san francisco, ca'
-    // };
 
-    // let searchRequest = JSON.stringify(searchParams);
+    // let searchRequest = JSON.stringify(searchParams); 
 
-    // let term = '';
-    // let location = '';
-    // let latitude = '';
-    // let longitude = '';
-    // let radius = '';
-    // let open_now = '';
-    // const limit = 10;
-
-    // axios.get(`/search`, searchRequest)
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch(function (err) {
-    //     console.log(err);
-    // });
