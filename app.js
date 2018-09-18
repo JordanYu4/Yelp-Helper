ac32b2090cd65950023abfa44be2c6f0440783cb
Jordan Yu <jordanyu44@gmail.com>
2018-09-18 15:05:08 -0700

    Remove extraneous code from index.js and app.js

--- app.js
+++ app.js
@@ -1,25 +1,87 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const app = express();
 const path = require('path');
-const fetch = require('node-fetch');
-const PORT = process.env.PORT || 3000; // process.env accesses heroku's environment variables
-const bodyParser = require('body-parser');
 
 const apiKey = require('./config/key');
+const yelp = require('yelp-fusion');
+const client = yelp.client(apiKey);
+
+// Search route 
+
+// app.get('/search', (request, response) => {
+  //
+  //   client.search(request).then(response => {
+    //     return response.jsonBody.businesses;
+    //   }).then(body => {
+      //     let cleanJson = JSON.stringify(body, null, 4);
+      //     // let results = JSON.parse(body);
+      //     console.log(cleanJson);
+      //     response.send(cleanJson);
+      //   }).catch(err => {
+        //     console.log(err);
+        //   })
+        //
+        // });
+            
+app.get('/search', (req, res) => {
+  let searchResults = [];
+  client.search(req.query).then(response => {
+    response.jsonBody.businesses.forEach(business => { // refactor w/ for loop
+      let searchResult = {
+        "name": business.name,
+        "url": business.url,
+        "reviewCount": business.review_count,
+        "rating": business.rating,
+        "address": business.location
+      };
+      searchResults.push(searchResult);
+    })
+    console.log("SEARCH RESULT START", searchResults, "SEARCH RESULT END");
+    res.send(searchResults);
+  }).catch(err => {
+    console.log(err);
+  });
+})
+
+/////////////////////////////
+
+// create route to get single book by its isbn
+app.get('/books/:isbn', (request, response) => {
+  // make api call using fetch
+  fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
+  .then((response) => {
+    return response.text();
+  }).then((body) => {
+    let results = JSON.parse(body)
+    console.log(results)   // logs to server
+    response.send(results) // sends to frontend
+  });
+});
+
+app.use(express.static("public"));
+
+app.get('/', (request, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+///////////////////////////////
+
+// process.env accesses heroku's environment variables
+const PORT = process.env.PORT || 3000; 
 
 // Body Parser middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // app.use(function (req, res) {
 //   res.setHeader('Content-Type', 'text/plain')
 //   res.write('you posted:\n')
 //   res.end(JSON.stringify(req.body, null, 2))
 // });
-
-const yelp = require('yelp-fusion');
-const client = yelp.client(apiKey);
 
 // client.search(searchRequest).then(response => {
   //   const firstResult = response.jsonBody.businesses[0];
@@ -28,68 +90,8 @@ const client = yelp.client(apiKey);
   // }).catch(err => {
     //   console.log(err);
     // });
-    
-    // app.get('/search', (request, response) => {
-      //
-      //   client.search(request).then(response => {
-        //     return response.jsonBody.businesses;
-        //   }).then(body => {
-          //     let cleanJson = JSON.stringify(body, null, 4);
-          //     // let results = JSON.parse(body);
-          //     console.log(cleanJson);
-          //     response.send(cleanJson);
-          //   }).catch(err => {
-            //     console.log(err);
-            //   })
-            //
-            // });
-            
-            // Rewriting app.get
-            
-            app.get('/search', (req, res) => {
-              let searchResults = [];
-              client.search(req.query).then(response => {
-                response.jsonBody.businesses.forEach(business => { // refactor w/ for loop
-                  let searchResult = {
-                    "name": business.name,
-                    "url": business.url,
-                    "reviewCount": business.review_count,
-                    "rating": business.rating,
-                    "address": business.location
-                  };
-                  searchResults.push(searchResult);
-                })
-                console.log("SEARCH RESULT START", searchResults, "SEARCH RESULT END");
-                res.send(searchResults);
-              }).catch(err => {
-                console.log(err);
-              });
-            })
-            
-            /////////////////////////////
-            
-            // create route to get single book by its isbn
-            app.get('/books/:isbn', (request, response) => {
-              // make api call using fetch
-              fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
-              .then((response) => {
-                return response.text();
-              }).then((body) => {
-                let results = JSON.parse(body)
-                console.log(results)   // logs to server
-                response.send(results) // sends to frontend
-              });
-            });
-            
-            app.use(express.static("public"));
-            
-            app.get('/', (request, res) => {
-              res.sendFile(path.join(__dirname, './public/index.html'))
-            });
-            ///////////////////////////////
-            
-            app.listen(PORT, () => {
-              console.log(__dirname);
-              console.log(`Listening on ${PORT}`)
-            })
-            
+
+app.listen(PORT, () => {
+  console.log(__dirname);
+  console.log(`Listening on ${PORT}`)
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,21 +3,49 @@ import yelpChart from './chart.js';
 
 import '../assets/stylesheets/index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
+const searchInput = form => ({
+  term: form[0].value,
+  location: form[1].value
+});
 
-    // let isbn = '0201558025';
-    // axios.get(`/books/${isbn}`)
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
+document.addEventListener('DOMContentLoaded', () => {
+  
+  let isbn = '0201558025';
+  axios.get(`/books/${isbn}`)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+  document.addEventListener("submit", e => {
+    e.preventDefault();
+    const searchForm = document.getElementById("search-form");
+    const newSearch = searchInput(searchForm); // declare as var outside of DOMContentLoaded?
+    console.log(newSearch);
+
+    // $.ajax({
+      //   url: "/yelp",
+      //   type: "get",
+      //   success: response => {
+        //     console.log("Retrieving response...");
+        //     return response;
+        //   }
     // });
 
-    let searchParams = {
-      term: 'four barrel coffee',
-      location: 'san francisco, ca'
-    };
+  });
+
+});
+      
+    // chart rendering logic?
+    
+    // let searchParams = {
+    //   term: 'four barrel coffee',
+    //   location: 'san francisco, ca'
+    // };
+
+    // let searchRequest = JSON.stringify(searchParams);
 
     // let term = '';
     // let location = '';
@@ -27,37 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // let open_now = '';
     // const limit = 10;
 
-    let searchRequest = JSON.stringify(searchParams);
-
-    axios.get(`/search`, searchRequest)
-    .then((response) => {
-        console.log(response);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
-    ////////////////////////
-    document.addEventListener("submit", e => {
-      e.preventDefault();
-      const searchForm = document.getElementById("search-form");
-      const newSearch = searchInput(searchForm); // declare as var outside of DOMContentLoaded?
-      $.ajax({
-        url: "/yelp",
-        type: "get",
-        success: response => {
-          console.log("Retrieving response...");
-          return response;
-        }
-      });
-
-      // chart rendering logic?
-
-    });
-
-    const searchInput = form => ({
-      location: form[0].value,
-      term: form[1].value
-    });
-
-})
+    // axios.get(`/search`, searchRequest)
+    // .then((response) => {
+    //     console.log(response);
+    // })
+    // .catch(function (err) {
+    //     console.log(err);
+    // });
