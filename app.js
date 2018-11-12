32bd0d993f9f36c3fe7b5298ac3d52df3f5dfa19
Jordan Yu <jordanyu44@gmail.com>
2018-11-12 12:28:58 -0800

    Temporarily deactivate open now backend functionality

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.post('/search', (request, response) => {
             price,
             distance, 
             review_count } = businesses[i];
+      let is_closed = false;
       let business = { name, 
                        image_url, 
                        url, 
@@ -33,8 +34,8 @@ app.post('/search', (request, response) => {
                        distance, 
                        is_closed,
                        review_count };
-      business.open_now = businesses[i].hours[0].is_open_now;
-      if (searchOptions.openNow && !business.open_now) continue;
+      // business.open_now = businesses[i].hours[0].is_open_now;
+      // if (searchOptions.openNow && !business.open_now) continue;
       if (!searchOptions[business.price]) continue;
       business.distance = ((business.distance / 1000) * 0.6213).toFixed(2);
       dataSet.push(business);
@@ -54,7 +55,7 @@ app.get('/', (request, res) => {
 });
 
 // process.env accesses heroku's environment variables
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 4000; 
 
 app.listen(PORT, () => {
   console.log(__dirname);
